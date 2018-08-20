bb313485d8236ca65fe0767e4bfc4acd3677bddc
Alexander Stannat <37674654+alexander-stannat@users.noreply.github.com>
2018-08-20 20:04:18 +0200

    Latest Updates

--- Convergence Testing.py
+++ Convergence Testing.py
@@ -1,13 +1,25 @@
+"""
+In this code we run the IncrementalPersonalizedPageRank class on a randomly generated graph to compute the
+personalized page rank values. We simultaneously the page rank method given in the networkx library and compare
+their values for different parameters.
+"""
 import networkx as nx
 import random
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from Page_Rank import IncrementalPersonalizedPageRank
 import numpy
 
+random.seed(111)
 graph = nx.DiGraph()
 number_of_nodes = random.randint(2, 100)
 nodes = range(number_of_nodes)
 graph.add_nodes_from(nodes)
-for _ in range(2*number_of_nodes):
+number_of_random_walks = 10
+random_walk_length = 5
+difference = list()
+
+for _ in range(2 * number_of_nodes):
     node_1 = random.choice(list(graph.nodes))
     node_2 = random.choice(list(set(graph.nodes) - {node_1}))
     if graph.has_edge(node_1, node_2) or graph.has_edge(node_2, node_1):
@@ -15,65 +27,54 @@ for _ in range(2*number_of_nodes):
     else:
         weight = random.randint(0, 10)
         graph.add_weighted_edges_from([(node_1, node_2, weight)])
-nx.draw_random(graph, node_size=30)
-        pr = IncrementalPersonalizedPageRank(self.graph, 0, 200, 0.05, 50)
+nx.draw_circular(graph, node_size=30, with_labels=True)
+plt.show()
+
+while number_of_random_walks <= 600:
+    number_of_random_walks += 200
+    random_walk_length = 5
+    while random_walk_length <= 100:
+        random_walk_length += 1
+        pr = IncrementalPersonalizedPageRank(graph, 0, number_of_random_walks, 0.05, random_walk_length)
         pr.initial_random_walks()
         page_ranks = pr.compute_personalized_page_ranks()
         page_ranks_2 = nx.pagerank(pr.graph, alpha=0.95, personalization={0: 1},
                                    max_iter=500, weight='weight')
-        print "Random Walks: ", pr.random_walks
 
-        print "Initial Page Ranks: ", "\n", page_ranks, "\n", page_ranks_2
-        #print numpy.linalg.norm(numpy.array(page_ranks.values()) - numpy.array(page_ranks_2.values()))
-        for node in pr.graph.nodes:
-            self.assertAlmostEqual(page_ranks[node], page_ranks_2[node], 1)
-        nx.draw_circular(self.graph, with_labels=True)
-        #plt.ion()
-        plt.show()
-        #plt.pause(5)
+        difference.append(numpy.linalg.norm(numpy.array(page_ranks.values()) - numpy.array(page_ranks_2.values())))
+plt.plot(range(6, 101), difference[0:95], 'r', range(6, 101), difference[96:191], 'g',
+         range(6, 101), difference[192:287], 'y')
+red_patch = mpatches.Patch(color='red', label='210 Random Walks')
+green_patch = mpatches.Patch(color='green', label='410 Random Walks')
+yellow_patch = mpatches.Patch(color='yellow', label='610 Random Walks')
+plt.legend(handles=[red_patch, green_patch, yellow_patch])
+plt.title('Accuracy of Page Ranks')
+plt.ylabel('Euclidean Norm')
+plt.xlabel('Random Walk Length')
+plt.show()
 
-        print "Initial Nodes :", self.graph.nodes
-        print "Initial Edges :", self.graph.edges
+difference_2 = []
+number_of_random_walks = 10
+random_walk_lengths = [25, 50, 100]
+for random_walk_length in random_walk_lengths:
+    while number_of_random_walks <= 500:
+        number_of_random_walks += 50
+        pr = IncrementalPersonalizedPageRank(graph, 0, number_of_random_walks, 0.05, random_walk_length)
+        pr.initial_random_walks()
+        page_ranks = pr.compute_personalized_page_ranks()
+        page_ranks_2 = nx.pagerank(pr.graph, alpha=0.95, personalization={0: 1},
+                                   max_iter=500, weight='weight')
 
-        c = random.randint(self.number_of_nodes//4, self.number_of_nodes//2)
-        choices = ["Remove Edge / Add Edge", "Change Edge Weight", "Remove Node / Add Node"]
-        for _ in range(c):
-            choice = random.choice(choices)
-            if choice == "Remove Edge / Add Edge":
-                node_1 = random.choice(list(pr.graph.nodes))
-                node_2 = random.choice(list(set(pr.graph.nodes) - {node_1}))
-                if pr.graph.has_edge(node_1, node_2):
-                    pr.remove_edge(node_1, node_2)
-                elif pr.graph.has_edge(node_2, node_1):
-                    pr.remove_edge(node_2, node_1)
-                else:
-                    weight = random.randint(-10, 10)
-                    pr.add_edge(node_1, node_2, weight)
-            elif choice == "Change Edge Weight":
-                edge = random.choice(list(pr.graph.edges))
-                weight = random.randint(-10, 10)
-                pr.add_weight_to_edge(edge[0], edge[1], weight)
-            elif choice == "Remove Node / Add Node":
-                w = random.randint(0, 1)
-                if w == 0:
-                    pr.add_node(self.number_of_nodes + 1)
-                    self.number_of_nodes += 1
-                elif w == 1:
-                    node = random.choice(list(set(pr.graph.nodes) - {0}))
-                    pr.remove_node(node)
+        difference.append(numpy.linalg.norm(numpy.array(page_ranks.values()) - numpy.array(page_ranks_2.values())))
+plt.plot(range(60, 510, 50), difference[0:9], 'r', range(60, 510, 50), difference[6:15], 'g',
+         range(60, 510, 50), difference[16:25], 'y')
+red_patch = mpatches.Patch(color='red', label='Random Walk Length: 25')
+green_patch = mpatches.Patch(color='green', label='Random Walk Length: 50')
+yellow_patch = mpatches.Patch(color='yellow', label='Random Walk Length: 100')
+plt.legend(handles=[red_patch, green_patch, yellow_patch])
+plt.title('Accuracy of Page Ranks')
+plt.ylabel('Euclidean Norm')
+plt.xlabel('Number of Random Walks')
+plt.show()
 
-        nx.draw_circular(self.graph, with_labels=True)
-        #plt.ion()
-        plt.show()
-        #plt.pause(5)
-        print "Updated Nodes :", self.graph.nodes
-        print "Updated Edges :", self.graph.edges
 
-        pr.update_random_walks()
-        new_page_ranks = pr.compute_personalized_page_ranks()
-        new_page_ranks_2 = nx.pagerank(pr.graph, alpha=0.95, personalization={0: 1},
-                                       max_iter=500, weight='weight')
-        print "New Random Walks: ", pr.random_walks
-        print "New Page Ranks: ", "\n", new_page_ranks, "\n", new_page_ranks_2
-        #print numpy.linalg.norm(numpy.array(new_page_ranks.values()) - numpy.array(new_page_ranks_2.values()))
-        #print page_ranks.keys() == page_ranks_2.keys()
--- main.py
+++ main.py
@@ -10,16 +10,16 @@ file_name = "multichain_09_02_18"
 
 gr = GraphReduction(path, file_name, 0, 2)
 gr.open_data_set()
-gr.generate_graph()
-nx.draw_random(gr.graph, node_size=30)
-plt.show()
+#gr.generate_graph()
+#nx.draw_random(gr.graph, node_size=30)
+#plt.show()
 node = random.choice(gr.blocks)[0]
 print len(gr.graph.nodes)
-pr = IncrementalPersonalizedPageRank(gr.graph, node, 1000, 0.05, 500)
+"""pr = IncrementalPersonalizedPageRank(gr.graph, node, 1000, 0.05, 500)
 pr.initial_random_walks()
 page_ranks = pr.compute_personalized_page_ranks()
 page_ranks_2 = nx.pagerank(gr.graph, alpha=0.95, personalization={node: 1},
-                           max_iter=500, weight='weight')
+                           max_iter=500, weight='weight')"""
 #print numpy.linalg.norm(numpy.array(page_ranks.values()) - numpy.array(page_ranks_2.values()))
 #print numpy.amax(numpy.array(page_ranks.values()) - numpy.array(page_ranks_2.values()))
 #print page_ranks.keys() == page_ranks_2.keys()
